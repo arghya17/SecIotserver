ff8d0741afe8da4045df46c5c7a49e90c24cb99e
arghya17 <arghyadeepsaha17@gmail.com>
2022-05-20 12:45:59 +0530

    Docker file changed apktool route added which works in this version mobsf route added which throws error results of mobsf and apktools were parsed successfully

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const checkAuth = require("./checkAuth");
 const nmap = require("./nmap");
 const metasploit = require("./metasploit");
 const spike = require("./spike");
-const static = require("./static");
+const { apktool, mobsf } = require("./static");
 const multer = require("multer");
 const fs = require("fs");
 const process = require("process");
@@ -19,11 +19,17 @@ router.post("/change-password", checkAuth, userLogin.changePassword); //checkAut
 router.post("/user/nmap", checkAuth, nmap);
 router.post("/user/metasploit", checkAuth, metasploit);
 router.post("/user/spike", checkAuth, spike);
-router.post("/user/static", checkAuth, upload.single("apk"), static, () => {
+router.post("/user/apktool", checkAuth, upload.single("apk"), apktool, () => {
   fs.rmSync(path.join(process.cwd(), "/uploads"), {
     recursive: true,
     force: true,
   });
 });
 //the form name should be apk which is used to upload the apk files
+router.post("/user/mobsf", checkAuth, upload.single("apk"), mobsf, () => {
+  fs.rmSync(path.join(process.cwd(), "/uploads"), {
+    recursive: true,
+    force: true,
+  });
+});
 module.exports = router;
